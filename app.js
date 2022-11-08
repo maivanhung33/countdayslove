9fd073358e80f83521c0a6f0adec1485ce716d29
maivanhung33 <maihung303@gmail.com>
2022-11-08 14:52:29 +0700

    init

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ document.addEventListener('DOMContentLoaded', function(){
             rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
       } olock();
       var timer = setInterval(function(){olock()}, 1000);
-      document.querySelector("audio").setAttribute("src", `music/ido.mp3`);
-
+//      document.querySelector("audio").setAttribute("src", `music/ido.mp3`);
+//
+//    var x = document.getElementById("audio");
+//    x.autoplay = true;
+//    x.load();
       document.getElementsByTagName("body")[0].insertAdjacentHTML(
             "beforeend",
             "<div id='mask'></div>"
